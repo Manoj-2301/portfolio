b6bf2cab5d9622479f3ba3dd059f34abd820eb42
Manoj-2301 <manojthapa.uiux@gmail.com>
2025-11-08 11:28:13 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
+// Global styles
+import "../app/resource/styles/global.scss";
+import "../app/resource/styles/reset.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
